731a47854f8cb566c44c419628eaab777947c7aa
Satyam Shekhar <satyam@scaligent.com>
2013-07-19 02:33:29 -0700

    Uniquify buffers.

--- src/dud-common.el
+++ src/dud-common.el
@@ -1,10 +1,15 @@
+(require 'whitespace)
+
 ;; Use to C-q C-i insert horizontal tabs.
 (setq-default indent-tabs-mode nil)
 
-(require 'whitespace)
-
 (delete-selection-mode t)
 (global-hl-line-mode t)
+
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'post-forward)
+(setq uniquify-after-kill-buffer-p t)
+(setq uniquify-ignore-buffers-re "^\\*")
 
 ;; (add-hook 'write-file-hooks 'delete-trailing-whitespace)
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
