1281985bab0d50eb08d3e25d7946ef11e36e6046
Satyam Shekhar <satyam@scaligent.com>
2013-07-20 01:36:19 -0700

    Remove redundant packages.

--- personal/dud-init.el
+++ personal/dud-init.el
@@ -1,8 +1,3 @@
-;; Set package repositories.
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")))
-
 ;; Set save directory for backup (~) files. Use backup-by-
 ;; copying. Slowest and safest method.
 (setq backup-directory-alist `(("." . "~/.saves")))
