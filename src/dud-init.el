98c5d83200cff019833da4ad288c2800c9ef32f3
Satyam Shekhar <satyam@scaligent.com>
2013-07-21 17:43:12 -0700

    Fix copyright in init file.

--- src/dud-init.el
+++ src/dud-init.el
@@ -1,5 +1,6 @@
-;; Dud is a very simple emacs configuration project meant to make
-;; configuration very simple and thus the name.
+;; Dud is emacs configuration for dummy's. It's meant to expose the
+;; power of emacs without much configuration and at the same time
+;; adhereing to emacs philosophy of customization.
 ;;
 ;;     Copyright (C) 2013  Satyam Shekhar
 ;;
