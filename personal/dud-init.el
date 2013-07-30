bdd629d4c365e411159460d1fdca862172c1bb53
Satyam Shekhar <satyam@scaligent.com>
2013-07-30 15:39:05 -0700

    Add copyright.

--- personal/dud-init.el
+++ personal/dud-init.el
@@ -1,6 +1,22 @@
-;; Set save directory for backup (~) files. Use backup-by-
-;; copying. Slowest and safest method.
-(setq backup-directory-alist `(("." . "~/.saves")))
-(setq backup-by-copying t)
+;; Dud is emacs configuration for dummy's. It's meant to expose the
+;; power of emacs without much configuration and at the same time
+;; adhereing to emacs philosophy of customization.
+;;
+;;     Copyright (C) 2013  Satyam Shekhar
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License
+;; as published by the Free Software Foundation; either version 2
+;; of the License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+;; 02110-1301, USA.
 
 (color-theme-ir-black)
