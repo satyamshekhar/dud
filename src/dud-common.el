d3a960edb5c3b2eb484b6e9452b2098c93b9f6bd
Satyam Shekhar <satyam@scaligent.com>
2013-07-30 15:39:46 -0700

    Save temporary files at other location.

--- src/dud-common.el
+++ src/dud-common.el
@@ -37,4 +37,18 @@
 (global-whitespace-mode t)
 (setq whitespace-style '(face trailing tabs lines-tail newline empty))
 
+(defvar user-temporary-file-directory
+  (concat temporary-file-directory user-login-name "/"))
+(make-directory user-temporary-file-directory t)
+(setq backup-by-copying t)
+(setq backup-directory-alist
+      `(("." . ,user-temporary-file-directory)
+        (,tramp-file-name-regexp nil)))
+(setq auto-save-list-file-prefix
+      (concat user-temporary-file-directory ".auto-saves-"))
+(setq auto-save-file-name-transforms
+      `((".*" ,user-temporary-file-directory t)))
+(setq backup-directory-alist `(("." . "~/.saves")))
+(setq backup-by-copying t)
+
 (provide 'dud-common)
