e66790d9ea02c69538e467d6736dfab1c4cd3fee
Satyam Shekhar <satyamshekhar@gmail.com>
2013-12-22 00:03:53 -0800

    Add mode for SConstruct

--- src/dud-common.el
+++ src/dud-common.el
@@ -62,4 +62,9 @@
 (setq backup-directory-alist `(("." . "~/.saves")))
 (setq backup-by-copying t)
 
+(setq auto-mode-alist
+      (cons '("SConstruct" . python-mode) auto-mode-alist))
+(setq auto-mode-alist
+      (cons '("SConscript" . python-mode) auto-mode-alist))
+
 (provide 'dud-common)
